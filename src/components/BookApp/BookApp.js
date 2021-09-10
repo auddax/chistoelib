96b77f24ee409be110fceb95d0161b832786d131
auddax <auddax@gmail.com>
2021-09-10 21:23:13 +0300

    Refactor BooksListApp and BookApp

--- src/components/BookApp/BookApp.js
+++ src/components/BookApp/BookApp.js
@@ -1,18 +1,41 @@
+import React, { useState, useEffect } from 'react';
+import fetchJsonp from 'fetch-jsonp';
 
 function BookApp(props) {
-  const { bookid, title, author } = props;
+  const { bookid } = props;
+  const [booksData, setBooksData] = useState({});
+
   const link = `https://www.litres.ru/pages/biblio_book/?art=${bookid}&lfrom=2886435`
   const linkImg = `https://cv2.litres.ru/pub/c/elektronnaya-kniga/cover_415/${bookid}-pages-biblio-book-art-${bookid}.jpg`
+
+  useEffect(() => {
+    let apiRequest = `https://www.litres.ru/pages/get_vidget_json/?art=${bookid}`;
+    fetchJsonp(apiRequest, { timeout: 10000 })
+    .then(function(response) {
+      return response.json()
+    }).then(function(json) {
+     setBooksData(
+        {
+          id: bookid,
+          title: json.biblio_book[0].['name'],
+          author: json.biblio_book[0].['s_author-name']
+        }
+      )
+    }).catch(function(ex) {
+      console.log('parsing failed', ex)
+    })
+  // eslint-disable-next-line react-hooks/exhaustive-deps  
+  }, []);
 
   return (
     <figure>
       <a href={link} target="_blank" rel="noreferrer">
         <div className="image-container">
-          <img src={linkImg} alt={title}/>
+          <img src={linkImg} alt={booksData.title}/>
         </div>
         <figcaption>
-          <p className="books-title">{title}</p>
-          <p className="books-author">{author}</p>
+          <p className="books-title">{booksData.title}</p>
+          <p className="books-author">{booksData.author}</p>
         </figcaption>
       </a>
     </figure>
--- src/components/BooksListApp/BooksListApp.js
+++ src/components/BooksListApp/BooksListApp.js
@@ -1,37 +1,15 @@
-import React, { useState, useEffect } from 'react';
-import fetchJsonp from 'fetch-jsonp';
+import React from 'react';
 import BookApp from '../BookApp/BookApp';
 
 function BooksListApp() {
   const booksID = [42189891, 31256891, 18223031, 9600888];
-  const [booksData, setBooksData] = useState([]);
 
-  useEffect(() => {
-    booksID.forEach(id => {
-      let apiRequest = `https://www.litres.ru/pages/get_vidget_json/?art=${id}`;
-      fetchJsonp(apiRequest, { timeout: 10000 })
-      .then(function(response) {
-        return response.json()
-      }).then(function(json) {
-        booksData.push({
-          id: id,
-          title: json.biblio_book[0].['name'],
-          author: json.biblio_book[0].['s_author-name']
-        })
-      }).catch(function(ex) {
-        console.log('parsing failed', ex)
-      })
-    })
-    setBooksData([...booksData]);
-  // eslint-disable-next-line react-hooks/exhaustive-deps  
-  }, []);
-  
   return (
     <section id="books-page">
       <h2 id="books-header">Сентябрь горит, но мы не плачем! Подборка книг для уютной осени.</h2>
       <div id="books-container">
-         {booksData.map(book => {
-          return <BookApp bookid={book.id} title={book.title} author={book.author} />
+         {booksID.map(id => {
+          return <BookApp bookid={id} />
          })}
       </div>
     </section>
