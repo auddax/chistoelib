f7645f5b587d20ad55ef776d1df37c79bc342bf9
auddax <auddax@gmail.com>
2021-09-17 23:05:52 +0300

    Add Litres library buttons in Books Page

--- src/components/BooksListApp/BooksListApp.js
+++ src/components/BooksListApp/BooksListApp.js
@@ -2,7 +2,7 @@ import React from 'react';
 import BookApp from '../BookApp/BookApp';
 
 function BooksListApp() {
-  const booksID = [42189891, 31256891, 18223031, 9600888];
+  const booksID = [42189891, 31256891, 18223031, 9600888, 24971845, 654875];
 
   return (
     <section id="books-page">
@@ -11,6 +11,10 @@ function BooksListApp() {
          {booksID.map(id => {
           return <BookApp bookid={id} />
          })}
+      </div>
+      <div id="books-buttons">
+        <div className="lib-button"><a href="https://biblio.litres.ru" target="_blank" rel="noreferrer">Перейти в библиотеку</a></div>
+        <div className="lib-button"><a href="https://biblio.litres.ru/libreg/1081039/" target="_blank" rel="noreferrer">Записаться в библиотеку</a></div>
       </div>
     </section>
   )    
