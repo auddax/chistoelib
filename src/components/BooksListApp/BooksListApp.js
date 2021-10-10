7b113396a19038fbbb22ffb88682bb353a3c42fd
auddax <auddax@gmail.com>
2021-10-10 16:35:17 +0300

    Reduce root font size

--- src/components/BooksListApp/BooksListApp.js
+++ src/components/BooksListApp/BooksListApp.js
@@ -2,11 +2,11 @@ import React from 'react';
 import BookApp from '../BookApp/BookApp';
 
 function BooksListApp() {
-  const booksID = [42189891, 31256891, 18223031, 9600888, 24971845, 654875];
+  const booksID = [596225, 121724, 166312, 125474, 144464];
 
   return (
     <section id="books-page">
-      <h2 id="books-header">Сентябрь горит, но мы не плачем! Подборка книг для уютной осени.</h2>
+      <h2 id="books-header">5 романов от осенней хандры</h2>
       <div id="books-container">
          {booksID.map(id => {
           return <BookApp bookid={id} />
